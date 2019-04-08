8aa25fd2847a96329205f7573a7a1ecd8ebd9bad
ramiro <ramiro.gordon@globant.com>
2019-04-08 15:46:21 -0300

    pasnf

--- spotify/src/components/Footer.js
+++ spotify/src/components/Footer.js
@@ -6,8 +6,7 @@ export class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <audio controls src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg">
-        </audio>
+        <audio controls src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"></audio>
       </div>
     )
   }
