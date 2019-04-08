a98d47ee6fcdf6401553d4cdf8eae0511ce62279
ramiro <ramiro.gordon@globant.com>
2019-04-08 10:56:03 -0300

    stated with homeScreen

---
+++ spotify/src/components/LibraryScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LibraryScreen extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default LibraryScreen;
---
+++ spotify/src/components/SearchScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SearchScreen extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SearchScreen;
