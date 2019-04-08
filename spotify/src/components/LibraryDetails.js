f835bb32cd5b84d73b7c6457436e5a0e073b9ac8
Julian Scerra <julian.scerra@globant.com>
2019-04-08 16:11:22 -0300

    library main divided

---
+++ spotify/src/components/LibraryDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LibraryDetails extends Component {
+  render() {
+    return (
+      <div>
+        DETAILS
+      </div>
+    )
+  }
+}
+
+export default LibraryDetails;
---
+++ spotify/src/components/LibraryNavigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class LibraryNavigation extends Component {
+  render() {
+    return (
+      <div>
+        NAVIGATION
+      </div>
+    )
+  }
+}
+
+export default LibraryNavigation;
