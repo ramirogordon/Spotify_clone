6937f3d60b9535ce8918c31e5ad3f152a7ebbb20
Julian Scerra <julian.scerra@globant.com>
2019-04-08 18:03:51 -0300

    some improvements in libraryNavigation

--- spotify/src/components/LibraryNavigation.js
+++ spotify/src/components/LibraryNavigation.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
+import '../styles/LibraryNavigation.css';
+
 export class LibraryNavigation extends Component {
   render() {
     return (
-      <div>
-        NAVIGATION
+      <div className="librarynavigation-content">
+        <input 
+          name="playlist"
+          value="Playlist"
+          type="button"/>
+        <input 
+          name="stations"
+          value="Stations"
+          type="button"/>
+        <input 
+          name="songs"
+          value="Songs"
+          type="button"/>
+        <input 
+          name="albums"
+          value="Albums"
+          type="button"/>
+        <input 
+          name="artists"
+          value="Artists"
+          type="button"/>
+        <input 
+          name="podcasts"
+          value="Podcasts"
+          type="button"/>
+        <input 
+          name="videos"
+          value="Videos"
+          type="button"/>
       </div>
     )
   }
