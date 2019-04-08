5ee892222d7a80a5c50796e4f51fd9338050a8bf
ramiro <ramiro.gordon@globant.com>
2019-04-08 11:58:47 -0300

    ksjnd

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -4,13 +4,24 @@ import SearchScreen from './components/SearchScreen';
 import LibraryScreen from './components/LibraryScreen';
 import './App.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/library">Library</Link>
+            </li>
+          </ul>
           <Route exact path="/" component={HomeScreen} />
           <Route path="/search" component={SearchScreen} />
           <Route path="/library" component={LibraryScreen} />
