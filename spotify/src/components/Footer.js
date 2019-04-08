9a6a5b5d260f100ae027c1f562347231a68cf561
ramiro <ramiro.gordon@globant.com>
2019-04-08 15:37:14 -0300

    started with audio

--- spotify/src/components/Footer.js
+++ spotify/src/components/Footer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import '../styles/Footer.css';
+import { Button, ButtonGroup } from '../components/buttons';
 
 export class Footer extends Component {
   render() {
     return (
-      <div>
-        FOOTER
+      <div className="footer">
+        <audio controls src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg">
+        </audio>
       </div>
     )
   }
