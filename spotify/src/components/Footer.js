cd968f78335ea30535182b70281e15fdcfa57066
ramiro <ramiro.gordon@globant.com>
2019-04-08 11:28:19 -0300

    footer and header

---
+++ spotify/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../styles/Footer';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Footer;
---
+++ spotify/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../styles/Header';
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Header;
