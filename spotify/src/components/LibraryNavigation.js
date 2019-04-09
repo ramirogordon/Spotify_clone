43c667d773dbcb611ce3108056891eca1d775558
Julian Scerra <julian.scerra@globant.com>
2019-04-09 09:57:44 -0300

    some changes

---
+++ spotify/src/components/LibraryNavigation.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import '../styles/LibraryNavigation.css';
+
+export class LibraryNavigation extends Component {
+  render() {
+    return (
+      <div className="librarynavigation-content">
+        <input 
+          name="playlist"
+          value="Playlist"
+          type="button"/>
+        <input 
+          name="stations"
+          value="Stations"
+          type="button"/>
+        <input 
+          name="songs"
+          value="Songs"
+          type="button"/>
+        <input 
+          name="albums"
+          value="Albums"
+          type="button"/>
+        <input 
+          name="artists"
+          value="Artists"
+          type="button"/>
+        <input 
+          name="podcasts"
+          value="Podcasts"
+          type="button"/>
+        <input 
+          name="videos"
+          value="Videos"
+          type="button"/>
+      </div>
+    )
+  }
+}
+
+export default LibraryNavigation;
